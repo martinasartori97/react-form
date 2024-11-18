67d80173a715162e51a491530e384fcbf9a8641d
martinasartori97 <martinasartori9@gmail.com>
2024-11-18 15:05:26 +0100

    edit js

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState(' React Blog Form')
 
   return (
     <>
-
+      <h1> React Blog Form</h1>
+      <p>insert the title</p>
+      <input type="text" title='title' id='title' value={title} onChange={e => setTitle(e.target.value)} />
 
     </>
   )
