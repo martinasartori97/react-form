2da001e97ec7b251703aff9224fb42ab50c837a7
martinasartori97 <martinasartori9@gmail.com>
2024-11-18 15:27:10 +0100

    edit js

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import './App.css'
 function App() {
   const [title, setTitle] = useState(' React Blog Form')
 
+  function handleTitleChange(e) {
+    console.log(e);
+    setTitle(e.target.value)
+
+  }
+
   return (
     <>
       <h1> React Blog Form</h1>
       <p>insert the title</p>
       <input type="text" title='title' id='title' value={title} onChange={e => setTitle(e.target.value)} />
+      <button type='submit'>send</button>
 
     </>
   )
